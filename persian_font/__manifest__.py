e51fd2fd243eb8b4f3b162bf643113b5ee09cd7c
mrr-r <66952858+mrr-r@users.noreply.github.com>
2021-03-09 18:44:57 +0330

    Add files via upload

---
+++ persian_font/__manifest__.py
@@ -0,0 +1,23 @@
+ # -*- coding: utf-8 -*-
+{
+    'name': "Persian font",
+
+    'summary': """
+        change defult font to nice persian font""",
+
+    'description': """
+        Change the defult persian font of the all interfaces with a beautiful one preferred by the Persian user
+ ,
+    """,
+    'author': "Maharaj",
+    'website': "http://odoo.com",
+    'category': 'Localization',
+    'version': '14.0.0.0',
+    'depends': ['web'],
+    'qweb': [],
+    'data': ['views/linkestatic.xml',   
+    ],
+    'license': 'LGPL-3',
+    'auto_install': True,
+    'installable': True,
+}
